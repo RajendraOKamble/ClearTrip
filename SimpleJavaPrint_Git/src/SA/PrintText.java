4d99e5b63e8e9f1df0627e9327fcf5e698bc0876
RajendraOKamble <raj.kambale@gmail.com>
2020-06-08 14:20:07 +0530

    commited code

---
+++ SimpleJavaPrint_Git/src/SA/PrintText.java
@@ -0,0 +1,10 @@
+package SA;
+
+public class PrintText {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("From Git...It is printing");
+	}
+
+}
